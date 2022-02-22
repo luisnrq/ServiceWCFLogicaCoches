b225b6b6629e33e528bef68f91deb5b0ddeab021
luisnrq <luisenrique.frias@tajamar365.com>
2022-02-22 13:43:44 +0100

    comit

---
+++ CochesClass.cs
@@ -0,0 +1,32 @@
+﻿using ServiceWCFLogicaCoches.Models;
+using ServiceWCFLogicaCoches.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceWCFLogicaCoches
+{
+    public class CochesClass : ICochesContract
+    {
+        private RepositoryCoches repo;
+
+        public CochesClass()
+        {
+            this.repo = new RepositoryCoches();
+        }
+
+        public List<Coche> GetCoches()
+        {
+            return this.repo.GetCoches();
+        }
+
+        public Coche FindCoche(int id)
+        {
+            return this.repo.FindCoche(id);
+        }
+
+        
+    }
+}
